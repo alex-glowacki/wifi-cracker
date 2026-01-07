ccc73af232b53cffdd2b71867c9d30361f67b5e8
alex-glowacki <alex.glowacki@acgservicesnd.com>
2026-01-06 21:15:30 -0600

    Refactored scan_window.py

---
+++ src/wifi_cracker/ui/constants.py
@@ -0,0 +1,6 @@
+# src/wifi_cracker/ui/constants.py
+
+# Constant variables
+SCAN_WINDOW_TITLE = "Scan Results"
+SCAN_WINDOW_SIZE = (250, 250)
+SCAN_BUTTON_TEXT = "Start Scan"
--- src/wifi_cracker/ui/scan_window.py
+++ src/wifi_cracker/ui/scan_window.py
@@ -5,42 +5,46 @@ from __future__ import annotations
 
 # Third-party
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QVBoxLayout,
-    QWidget,
-    QLabel,
-)
+from PyQt6.QtWidgets import QVBoxLayout, QWidget
+
 
 # Local application
-import wifi_cracker.ui.widgets.buttons as Btn
 import wifi_cracker.core.wifi_scan as WifiScan
+import wifi_cracker.ui.widgets.buttons as Btn
 import wifi_cracker.ui.widgets.text_contents as TextContents
+from wifi_cracker.ui.constants import (
+    SCAN_WINDOW_TITLE,
+    SCAN_WINDOW_SIZE,
+    SCAN_BUTTON_TEXT,
+)
+
 
 # Classes
 class ScanWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Scan Results")
-        self.resize(250, 250)
+        
+        self.setWindowTitle(SCAN_WINDOW_TITLE)
+        self.resize(*SCAN_WINDOW_SIZE)
         
         layout = QVBoxLayout()
         
-        scan_btn_text = "Start Scan"
-        self.scan_btn = Btn.var_btn(scan_btn_text)
+        self.scan_btn = Btn.var_btn(SCAN_BUTTON_TEXT)
         self.scan_btn.clicked.connect(self._update_scan_results)
         layout.addWidget(self.scan_btn, alignment=Qt.AlignmentFlag.AlignTop)
         
-        # Eventually make this a list that will connect to the GUI main window text box.
         ssids = WifiScan.scan_ssids()
+        
         self.scan_results = TextContents.text_content(ssids)
         self.scan_results.setText("\n".join(ssids))
-        self.scan_results.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-        layout.addWidget(self.scan_results)
+        self.scan_results.setAlignment(
+            Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop
+        )
         
+        layout.addWidget(self.scan_results)
         self.setLayout(layout)
+        
     
     def _update_scan_results(self):
-        new_scan_results = WifiScan.scan_ssids()
-        self.scan_results.setText("\n".join(new_scan_results))
+        ssids = WifiScan.scan_ssids()
+        self.scan_results.setText("\n".join(ssids))
